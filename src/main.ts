13c68691fc4514b87604dc34cc4a27d611ffef3d
Michael Suo <suo@fb.com>
2019-09-06 16:04:17 -0700

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,93 +1,94 @@
 import * as core from '@actions/core';
 import * as exec from '@actions/exec';
 import * as github from '@actions/github';
-import * as io from '@actions/io';
+import * as octokit from '@octokit/rest';
 
 const { GITHUB_TOKEN } = process.env;
-const checkName = "flake8 lint"
 
 async function runFlake8() {
-  await exec.exec('pip3', ['install', 'flake8']);
   let myOutput = '';
-  let myError = '';
   let options = {
-      listeners: {
-          stdout: (data: Buffer) => {
-              myOutput += data.toString();
-          },
-          stderr: (data: Buffer) => {
-              myError += data.toString();
-          }
-      }
+    listeners: {
+      stdout: (data: Buffer) => {
+        myOutput += data.toString();
+      },
+    }
   };
-  const flake8Path: string = await io.which('flake8', true)
-  await exec.exec(`"${flake8Path}"`, ['main.py']);
+  await exec.exec('flake8 --exit-zero', [], options);
   return myOutput;
 }
 
-function parseOutput(output) {
-    // Group 0: whole match
-    // Group 1: filename
-    // Group 2: line number
-    // Group 3: column number
-    // Group 4: error code
-    // Group 5: error description
-    let regex = new RegExp(/^(.*?):(\d+):(\d+): (\w\d+) (.*)$/);
-    let errors = output.split('\n');
-    let matches : any[] = [];
-    for (let i = 0; i < errors.length; i++) {
-        let error = errors[i];
-        let match = error.match(regex);
-        if (match) {
-          match = {
-            path: match[1],
-            start_line: match[2],
-            end_line: match[2],
-            start_column: match[3],
-            end_column: match[3],
-            annotation_level: "failure",
-            message: `[${match[4]}] ${match[5]}`,
-          };
+type Annotation = octokit.ChecksUpdateParamsOutputAnnotations;
+// Regex the output for error lines, then format them in
+function parseFlake8Output(output: string): Annotation[] {
+  // Group 0: whole match
+  // Group 1: filename
+  // Group 2: line number
+  // Group 3: column number
+  // Group 4: error code
+  // Group 5: error description
+  let regex = new RegExp(/^(.*?):(\d+):(\d+): (\w\d+) ([\s|\w]*)/);
+  let errors = output.split('\n');
+  let annotations: Annotation[] = [];
+  for (let i = 0; i < errors.length; i++) {
+    let error = errors[i];
+    let match = error.match(regex);
+    if (match) {
+      // Chop `./` off the front so that Github will recognize the file path
+      const normalized_path = match[1].replace('./', '');
+      const line = parseInt(match[2]);
+      const column = parseInt(match[3]);
+      const annotation_level = <const> 'failure';
+      const annotation = {
+        path: normalized_path,
+        start_line: line,
+        end_line: line,
+        start_column: column,
+        end_column: column,
+        annotation_level,
+        message: `[${match[4]}] ${match[5]}`,
+      };
 
-          matches.push(match);
-      }
+      annotations.push(annotation);
     }
-
-    const any_matches = matches.length > 0;
-    return {
-      conclusion: any_matches ? 'failure' : 'success',
-      output: {
-        title: checkName,
-        summary: `${matches.length} error(s) found`,
-        matches,
-      },
-
-    };
+  }
+  return annotations;
 }
 
-async function createCheck(checkData) {
+async function createCheck(check_name: string, title: string, annotations: Annotation[]) {
   const octokit = new github.GitHub(String(GITHUB_TOKEN));
-  await octokit.checks.create({
+  const res = await octokit.checks.listForRef({
+    check_name,
     ...github.context.repo,
-    name: checkName,
-    sha: github.context.sha,
-    ...checkData
+    ref: github.context.sha
+  });
+
+  const check_run_id = res.data.check_runs[0].id;
+
+  await octokit.checks.update({
+    ...github.context.repo,
+    check_run_id,
+    output: {
+      title,
+      summary: `${annotations.length} errors(s) found`,
+      annotations
+    }
   });
 }
 
-
 async function run() {
   try {
-      const myInput = core.getInput('myInput');
-      const flake8Output = await runFlake8();
-      const checkData = parseOutput(flake8Output);
-      await createCheck(checkData);
-      if (checkData.conclusion === 'failure') {
-        core.setFailed('flake8 failures found');
-      }
+    const flake8Output = await runFlake8();
+    const annotations = parseFlake8Output(flake8Output);
+    if (annotations.length > 0) {
+      console.log(annotations);
+      const checkName = core.getInput('checkName');
+      await createCheck(checkName, "flake8 failure", annotations);
+      core.setFailed(`${annotations.length} errors(s) found`);
+    }
   }
-    catch (error) {
-      core.setFailed(error.message);
+  catch (error) {
+    core.setFailed(error.message);
   }
 }
 
